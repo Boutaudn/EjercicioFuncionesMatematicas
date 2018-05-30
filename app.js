22cffcb5d3bdcc5efb4ede86466a2b5d912dd6ea
Boutaudn <boutaudn@gmail.com>
2018-05-29 21:04:25 -0400

    ejercicio subido de una

---
+++ app.js
@@ -0,0 +1,11 @@
+
+const add = function (x, y) {
+    return x + y;
+ }
+
+const multiply = function (x, y) {
+    return x * y;
+ }
+ 
+let resultado = multiply(36325, add(9824, 777));{
+console.log(resultado);}
